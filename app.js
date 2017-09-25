c2c4d82cbfff3ea6d5f90a1d4642f8db28af8ea3
langrand <ludo.langrand@gmail.com>
2017-09-25 11:09:51 +0200

    add ejs

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-
+app.set('view engine', 'ejs');
 
 app.use("/web", express.static(__dirname + "/web"));
 
 app.get('/', function (req, res) {
-    res.render('index.ejs');
+    res.render('index');
 });
 
 app.listen(8080);
