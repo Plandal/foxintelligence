294b06190f018a406d4c974aa1daed7d225c9670
langrand <ludo.langrand@gmail.com>
2017-09-25 21:37:35 +0200

    first functional version

--- web/js/index.js
+++ web/js/index.js
@@ -4,7 +4,6 @@ console.log('load js oki')
 $('#fiHtmlFile').on('change', function (e) {
     let file = $(this).get(0).files[0];
     if (file && file.type === "text/html") {
-        var json = $.fn.fiHtmlToJson(file,$('#temp'));
-        console.log('json = ',json);
+        $.fn.fiHtmlToJson(file);
     }
 });
